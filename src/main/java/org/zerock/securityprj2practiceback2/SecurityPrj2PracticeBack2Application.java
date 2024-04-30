3ddc796094265145c0e4c846fb1ea396d9c452c9
Garbi93 <iseung2093@gmail.com>
2024-04-30 13:05:13 +0900

    4/30 first commit

---
+++ src/main/java/org/zerock/securityprj2practiceback2/SecurityPrj2PracticeBack2Application.java
@@ -0,0 +1,13 @@
+package org.zerock.securityprj2practiceback2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPrj2PracticeBack2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityPrj2PracticeBack2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/securityprj2practiceback2/SecurityPrj2PracticeBack2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.securityprj2practiceback2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPrj2PracticeBack2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
