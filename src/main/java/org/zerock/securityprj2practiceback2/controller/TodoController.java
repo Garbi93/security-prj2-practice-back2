671daed309418888521e364e1c2e120a0941cea6
Garbi93 <iseung2093@gmail.com>
2024-05-03 16:01:36 +0900

    5/03 Rest 방식 컨트롤러 만들기 (2) -> Formatter를 만들고 TodoController 에 PostMapping을 추가 하여 post 기능 테스트

--- src/main/java/org/zerock/securityprj2practiceback2/controller/TodoController.java
+++ src/main/java/org/zerock/securityprj2practiceback2/controller/TodoController.java
@@ -2,14 +2,13 @@ package org.zerock.securityprj2practiceback2.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.securityprj2practiceback2.dto.PageRequestDTO;
 import org.zerock.securityprj2practiceback2.dto.PageResponseDTO;
 import org.zerock.securityprj2practiceback2.dto.TodoDTO;
 import org.zerock.securityprj2practiceback2.service.TodoService;
+
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -30,5 +29,14 @@ public class TodoController {
         log.info("list............" + pageRequestDTO);
 
         return todoService.getList(pageRequestDTO);
+    }
+
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDTO dto ) {
+        log.info("todoDTo: " + dto);
+
+        Long tno = todoService.register(dto);
+
+        return Map.of("TNO", tno);
     }
 }
