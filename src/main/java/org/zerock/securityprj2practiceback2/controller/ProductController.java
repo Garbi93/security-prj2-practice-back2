e37a8d0fbb0161ea80e72688fc9aa59e30ef68cd
Garbi93 <iseung2093@gmail.com>
2024-05-05 22:09:34 +0900

    5/05 업로드 파일 조회/삭제

--- src/main/java/org/zerock/securityprj2practiceback2/controller/ProductController.java
+++ src/main/java/org/zerock/securityprj2practiceback2/controller/ProductController.java
@@ -2,9 +2,9 @@ package org.zerock.securityprj2practiceback2.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.zerock.securityprj2practiceback2.dto.ProductDTO;
 import org.zerock.securityprj2practiceback2.util.CustomFileUtil;
@@ -34,6 +34,13 @@ public class ProductController {
         log.info(uploadedFileNames);
 
         return Map.of("RESULT", "SUCCESS");
+    }
+
+    @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFileGET(@PathVariable("fileName") String fileName) {
+
+        return fileUtil.getFile(fileName);
+
     }
 
 }
