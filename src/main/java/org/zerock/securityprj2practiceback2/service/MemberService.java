c41e4e2c7f01fac8072678af089628766ef75247
Garbi93 <iseung2093@gmail.com>
2024-05-10 16:38:01 +0900

    5/10 API 서버에서 소셜 회원 처리

--- src/main/java/org/zerock/securityprj2practiceback2/service/MemberService.java
+++ src/main/java/org/zerock/securityprj2practiceback2/service/MemberService.java
@@ -2,11 +2,26 @@ package org.zerock.securityprj2practiceback2.service;
 
 
 import org.springframework.transaction.annotation.Transactional;
+import org.zerock.securityprj2practiceback2.domain.Member;
 import org.zerock.securityprj2practiceback2.dto.MemberDTO;
+
+import java.util.stream.Collectors;
 
 @Transactional
 public interface MemberService {
 
     MemberDTO getKakaoMember(String accessToken);
 
+
+    default MemberDTO entityToDTO(Member member) {
+
+        MemberDTO dto = new MemberDTO(
+                member.getEmail(),
+                member.getPw(),
+                member.getNickname(),
+                member.isSocial(),
+                member.getMemberRoleList().stream().map(memberRole -> memberRole.name()).collect(Collectors.toList()));
+
+        return dto;
+    }
 }
--- src/main/java/org/zerock/securityprj2practiceback2/service/MemberServiceImpl.java
+++ src/main/java/org/zerock/securityprj2practiceback2/service/MemberServiceImpl.java
@@ -9,14 +9,18 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.zerock.securityprj2practiceback2.domain.Member;
+import org.zerock.securityprj2practiceback2.domain.MemberRole;
 import org.zerock.securityprj2practiceback2.dto.MemberDTO;
 import org.zerock.securityprj2practiceback2.repository.MemberRepository;
 
 import java.util.LinkedHashMap;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,23 +29,61 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     @Value("${kakao.get.user.url}")
     private String kakaoGetUser;
 
     @Override
     public MemberDTO getKakaoMember(String accessToken) {
 
-        // 1. accessToken 을 이용해서 사용자 정보 가져오기
-        getEmailFromKakaoAccessToken(accessToken);
+        // 1. accessToken 을 이용해서 사용자 이메일 정보 가져오기
+        String email = getEmailFromKakaoAccessToken(accessToken);
 
-        // 기존 DB에 회원 정보가 있는 경우 / 없는 경우
+        // 기존 DB에 회원 정보가 있는 경우
+        Optional<Member> result = memberRepository.findById(email);
+        if (result.isPresent()){
 
-        return null;
+            MemberDTO memberDTO = entityToDTO(result.get());
+
+            log.info("existed...................." + memberDTO);
+
+            return memberDTO;
+        }
+
+        //---------------------------------------------------------------------
+
+        // 없는 경우 -> 회원 만들기
+        Member socialMember = makeSocialMember(email);
+        memberRepository.save(socialMember);
+
+        MemberDTO memberDTO = entityToDTO(socialMember);
+
+        return memberDTO;
+    }
+
+    // 기존 DB 에 동일한 회원 정보가 없는 경우 -> 카카오 회원 생성 시켜주기
+    private Member makeSocialMember(String email) {
+
+        String tempPassword = makeTempPassword();
+
+        log.info("tempPassword: " + tempPassword);
+
+        Member member = Member.builder()
+                .email(email)
+                .pw(passwordEncoder.encode(tempPassword))
+                .nickname("Social Member")
+                .social(true)
+                .build();
+
+        member.addRole(MemberRole.USER);
+
+        return member;
     }
 
 
     // 카카오에서 정보 가져오기
-    private void getEmailFromKakaoAccessToken(String accessToken) {
+    private String getEmailFromKakaoAccessToken(String accessToken) {
 
         String kakaoGetUserURL = kakaoGetUser;
 
@@ -58,14 +100,33 @@ public class MemberServiceImpl implements MemberService {
         ResponseEntity<LinkedHashMap> response =
                 restTemplate.exchange(uriBuilder.toUri(), HttpMethod.GET, entity, LinkedHashMap.class);
 
+        log.info("response --------------------------------");
         log.info(response);
 
         LinkedHashMap<String, LinkedHashMap> bodyMap = response.getBody();
 
-        log.info("response --------------------------------");
-        log.info(bodyMap);
+        LinkedHashMap<String, String> kakaoAccount = bodyMap.get("kakao_account");
 
+        log.info("kakaoAccount: --- " + kakaoAccount);
+
+        String email = kakaoAccount.get("email");
+
+        log.info("email: " + email);
+
+        return email;
     }
+
+    private String makeTempPassword() {
+
+        StringBuffer buffer = new StringBuffer();
+
+        for (int i = 0; i < 10; i++) {
+            buffer.append((char)((int)(Math.random() * 55) + 65));
+        }
+        return buffer.toString();
+    }
+
+
 }
 
 
