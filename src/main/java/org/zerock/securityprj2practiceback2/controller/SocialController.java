2ae66c3bc1b5b58e9649c70966dcf3491e713706
Garbi93 <iseung2093@gmail.com>
2024-05-10 17:22:28 +0900

    5/10 API 서버와 리액트 연동 확인

--- src/main/java/org/zerock/securityprj2practiceback2/controller/SocialController.java
+++ src/main/java/org/zerock/securityprj2practiceback2/controller/SocialController.java
@@ -4,7 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.securityprj2practiceback2.dto.MemberDTO;
 import org.zerock.securityprj2practiceback2.service.MemberService;
+import org.zerock.securityprj2practiceback2.util.JWTUtil;
+
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -14,15 +18,23 @@ public class SocialController {
     private final MemberService memberService;
 
     @GetMapping("/api/member/kakao")
-    public String[] getMemberFromKakao(String accessToken) {
+    public Map<String, Object> getMemberFromKakao(String accessToken) {
 
         log.info("accessToken: " + accessToken);
 
-        memberService.getKakaoMember(accessToken);
+        MemberDTO memberDTO = memberService.getKakaoMember(accessToken);
 
-        return new String[]{"AAA", "BBB", "CCC"};
+        Map<String, Object> claims = memberDTO.getClaims();
 
+        String jwtAccessToken = JWTUtil.generateToken(claims, 10);
+        String jwtRefreshToken = JWTUtil.generateToken(claims, 60 * 24);
 
+        claims.put("accessToken", jwtAccessToken);
+        claims.put("refreshToken", jwtRefreshToken);
+
+        return claims;
     }
+
+
 
 }
