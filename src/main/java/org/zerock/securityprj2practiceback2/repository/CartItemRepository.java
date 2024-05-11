5a8601009c9e570823b515b6dcdd19fec8cdab66
Garbi93 <iseung2093@gmail.com>
2024-05-11 23:47:22 +0900

    5/11 Repository 개발 및 테스트 (1)

--- src/main/java/org/zerock/securityprj2practiceback2/repository/CartItemRepository.java
+++ src/main/java/org/zerock/securityprj2practiceback2/repository/CartItemRepository.java
@@ -23,10 +23,35 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
             "order by ci.cino desc ")
     List<Object[]> getItemsOfCartDTOByEmail(@Param("email") String email);
 
-    // 이메일, 상품 번호로 해당 상품이 장바구니 아이템 으로 존재 하는지 확인 필요
 
-    // 장바구니 아이템 번호를 장바구니를 얻어오려 하는 경우
+    // 이메일, 상품 번호로 해당 상품이 장바구니 아이템 으로 존재 하는지 확인 필요
+    @Query("select ci from CartItem ci left join Cart  c on ci.cart = c " +
+            "where c.owner.email = :email and ci.product.pno = :pno")
+    CartItem getItemOfPno(@Param("email") String email, @Param("pno") Long pno);
+
+
+    // 장바구니 아이템 번호를 장바구니 번호를 얻어오려 하는 경우
+    @Query("select c.cno from Cart c left join CartItem ci on ci.cart = c " +
+            "where ci.cino = :cino")
+    Long getCartFromItem(@Param("cino") Long cino);
+
 
     // 장바구니 번호로 모든 장바구니 아이템들 조회
+    @Query("select " +
+            "new org.zerock.securityprj2practiceback2.dto.CartItemListDTO(ci.cino, ci.qty, p.pname, p.price, pi.fileName ) " +
+            "from " +
+            "CartItem ci inner join Cart mc on ci.cart = mc " +
+            "left join Product p on ci.product = p " +
+            "left join p.imageList pi " +
+            "where " +
+            "pi.ord = 0 and mc.cno = :cno " +
+            "order by ci.cino desc ")
+    List<CartItemListDTO> getItemsOfCartDTOByCart(@Param("cno") Long cno);
+
+
+
+
+
+
 
 }
--- src/test/java/org/zerock/securityprj2practiceback2/repository/CartRepositoryTests.java
+++ src/test/java/org/zerock/securityprj2practiceback2/repository/CartRepositoryTests.java
@@ -4,6 +4,11 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.securityprj2practiceback2.domain.CartItem;
+
+import java.util.Optional;
 
 @SpringBootTest
 @Log4j2
@@ -14,6 +19,30 @@ public class CartRepositoryTests {
 
     @Autowired
     private CartItemRepository cartItemRepository;
+
+    @Transactional
+    @Commit
+    @Test
+    public void testInsertByProduct() {
+
+        String email = "user1@aaa.com";
+        Long pno = 5L;
+        int qty = 1;
+
+        // 이메일과 상품 번호로 장바구니 아이템 확인 -> 없으면 새로 추가 -> 있으면 수량 변경 해주기
+
+        CartItem cartItem = cartItemRepository.getItemOfPno(email, pno);
+
+        // 이미 사용자의 장바구니에 담겨 있다면 ???
+        if (cartItem != null) {
+            cartItem.changeQty(qty);
+            cartItemRepository.save(cartItem);
+            return;
+        }
+
+        // 장바구니에 담겨 있지 않다면 ???
+
+    }
 
     @Test
     public void testListOfMember() {
